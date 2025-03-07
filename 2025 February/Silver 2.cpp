6c8172d728b99d0386cc364bc4727a04720b40b6
Samuel-LY-Wang <samuellywang@gmail.com>
2025-03-06 21:35:57 -0500

    added basic code template to all the cpp files

---
+++ 2025 February/Silver 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <string>
+using namespace std;
+#pragma warning(suppress: 4267)
+
+int main() {
+  return 0;
+}
